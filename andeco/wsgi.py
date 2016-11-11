11f0dbddc8edd897f1e779c41286700147326456
noel <noel_v@hotmail.es>
2016-11-11 18:22:21 -0400

    servidor staticos

--- andeco/wsgi.py
+++ andeco/wsgi.py
@@ -13,4 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "andeco.settings")
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+
+from dj_static import Cling
+application = Cling(get_wsgi_application())
