c7cb7c2644791d71284c58f7a03ded62c7c8af91
noel <noel_v@hotmail.es>
2016-12-05 18:37:43 -0400

    usuarios

--- andeco/settings.py
+++ andeco/settings.py
@@ -84,23 +84,23 @@ WSGI_APPLICATION = 'andeco.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'amdeco',
-        'USER': 'root',
-        'PASSWORD': '1',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql', 
+#         'NAME': 'amdeco',
+#         'USER': 'root',
+#         'PASSWORD': '1',
+#         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Internationalization
